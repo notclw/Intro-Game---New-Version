55f8fa2d0e4adf34ffacb02b3c8c2cee21c3fb29
notclw <limchr@edu.emsb.qc.ca>
2025-04-07 15:00:16 -0400

    a

--- coin.gd
+++ coin.gd
@@ -9,10 +9,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	while true:
-		$MeshInstance3D.rotate_x(deg_to_rad(2))
-		$MeshInstance3D.rotate_y(deg_to_rad(2))
-		$MeshInstance3D.rotate_z(deg_to_rad(2))
+	$MeshInstance3D.rotate_x(deg_to_rad(2))
+	$MeshInstance3D.rotate_y(deg_to_rad(2))
+	$MeshInstance3D.rotate_z(deg_to_rad(2))
 
 
 func _on_body_entered(body):
